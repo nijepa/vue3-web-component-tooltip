b7baf72cc55f014e1b5af4cfa652ee00caa9b2be
Nikola Pavicevic <nijepa@gmail.com>
2022-12-19 19:18:01 +0100

    without inline-block

--- components/Custom-Tooltip.ce.vue
+++ components/Custom-Tooltip.ce.vue
@@ -1,35 +1,50 @@
 <template>
-	<transition name="wobble" appear >
-		<component
-			:is="isAbbreviation ? 'abbr' : 'span'"
-			:data-label="labelText"
-			:aria-label="labelText"
-			role="tooltip"
-			:class="[
-				hasPosition,
-				hasSize,
-				{
-					'vue-custom-tooltip': isActive && labelText,
-					'is-sticky': isSticky,
-					'has-multiline': isMultiline,
-					'is-underlined': isUnderlined || isAbbreviation,
-				},
-			]"
-			:style="{ cursor: isAbbreviation ? 'help' : 'pointer' }"
-		>
-			<slot name="default"></slot>
-		</component>
-  </transition>
+  <component
+    class="tooltip-container"
+    :is="isAbbreviation ? 'abbr' : 'span'"
+    :class="{ 'is-underlined': isUnderlined || isAbbreviation }"
+    :style="{ cursor: isAbbreviation ? 'help' : 'pointer' }"
+  >
+    <span
+      :class="[
+        hasPosition,
+        hasSize,
+        hasShadow,
+        {
+          'cadooz-tooltip': isActive && labelText,
+          'is-sticky': isSticky,
+          'has-multiline': isMultiline,
+          'is-underlined': isUnderlined || isAbbreviation,
+        },
+      ]"
+    >
+      <span :class="{ 'not-active': !isActive || !labelText }">{{
+        labelText
+      }}</span>
+      <svg
+        width="10"
+        height="10"
+        :class="[
+          hasPosition,
+          hasShadow,
+          { 'not-active': !isActive || !labelText },
+        ]"
+        class="cadooz-tooltip__arrow"
+      >
+        <rect width="10" height="10" />
+      </svg>
+    </span>
+    <slot />
+  </component>
 </template>
-
 <script setup>
-import { ref, computed, watch, useAttrs } from "vue";
+import { ref, computed, watch, onMounted, useAttrs } from "vue";
 
 // setting props
 const props = defineProps({
   active: {
     type: String,
-    default: 'true',
+    default: "true",
   },
   label: {
     type: String,
@@ -48,458 +63,210 @@ const props = defineProps({
   },
   position: {
     type: String,
-    default: 'is-top',
+    default: "is-top",
     validator(value) {
-      return ['is-top', 'is-bottom', 'is-left', 'is-right'].indexOf(value) > -1
+      return ["is-top", "is-bottom", "is-left", "is-right"].indexOf(value) > -1;
     },
   },
   size: {
     type: String,
-    default: 'is-medium',
+    default: "is-medium",
     validator(value) {
-      return ['is-small', 'is-medium', 'is-large'].indexOf(value) > -1
+      return ["is-small", "is-medium", "is-large"].indexOf(value) > -1;
     },
   },
   background: {
     type: String,
-    default: '#fff'
+    default: "#fff",
   },
   color: {
     type: String,
-    default: '#000'
+    default: "#000",
   },
   font: {
     type: String,
-    default: "'Open Sans', sans-serif"
+    default: "'Open Sans', sans-serif",
   },
-	weight: {
-		type: Number,
-		default: 400
-	},
-	radius: {
-		type: String,
-		default: '4px'
-	}
+  weight: {
+    type: Number,
+    default: 400,
+  },
+  radius: {
+    type: String,
+    default: "4px",
+  },
+  shadow: {
+    type: Boolean,
+    default: true,
+  },
 });
-const labelText = ref(props.label || null)
-const isActive = ref(props.active === 'true')
-const isSticky = ref(props.sticky || false)
-const isMultiline = ref(props.multiline || false)
-const isUnderlined = ref(props.underlined || false)
-const isAbbreviation = ref(props.abbreviation || false)
-const hasPosition = ref(props.position || 'is-top')
-const hasSize = ref(props.size || 'is-medium')
-const background = ref(props.background)
-const color = ref(props.color)
-const font = ref(props.font)
-const radius = ref(props.radius)
-console.log('rr', isActive.value)
+
+const attrs = useAttrs();
+const elem = ref(document.querySelector("#" + attrs.id));
+
+const labelText = ref(props.label || null);
+const isActive = ref(props.active === "true");
+const isSticky = ref(props.sticky || false);
+const isMultiline = ref(props.multiline || false);
+const isUnderlined = ref(props.underlined || false);
+const isAbbreviation = ref(props.abbreviation || false);
+const hasPosition = ref(props.position || "is-top");
+const hasSize = ref(props.size || "is-medium");
+const background = ref(props.background);
+const color = ref(props.color);
+const font = ref(props.font);
+const radius = ref(props.radius);
+
+const height = ref(null);
+const width = ref(null);
+
+const hasShadow = computed(() =>
+  props.shadow === "false" ? "" : "has-shadow"
+);
+
+onMounted(() => {
+  height.value = elem.value?.children[0]?.scrollHeight + "px";
+  width.value = elem.value?.children[0]?.scrollWidth + "px";
+});
 
 watch(
   () => props.active,
   (newValue, oldValue) => {
-    console.log("Watch props.active function called with args:", newValue, oldValue);
-    isActive.value = newValue;
+    console.info(
+      "Watch props.active function called with args:",
+      newValue,
+      oldValue
+    );
+    isActive.value = newValue === 'true';
   }
 );
-// const dynamicStyles = computed(() => {
-//   return {
-//     '--vue-custom-tooltip-color':
-//       $vueCustomTooltip && $vueCustomTooltip.hasOwnProperty('color')
-//         ? $vueCustomTooltip.color
-//         : null,
-//     '--vue-custom-tooltip-background':
-//       $vueCustomTooltip && $vueCustomTooltip.hasOwnProperty('background')
-//         ? $vueCustomTooltip.background
-//         : null,
-//     '--vue-custom-tooltip-border-radius':
-//       $vueCustomTooltip && $vueCustomTooltip.hasOwnProperty('borderRadius')
-//         ? $vueCustomTooltip.borderRadius
-//         : null,
-//     '--vue-custom-tooltip-font-weight':
-//       $vueCustomTooltip && $vueCustomTooltip.hasOwnProperty('fontWeight')
-//         ? $vueCustomTooltip.fontWeight
-//         : null,
-//   }
-// })
-</script>
-<!-- <script>
-export default {
-  name: 'VueCustomTooltip',
-  props: {
-    label: String,
-    active: {
-      type: Boolean,
-      default: true,
-    },
-    sticky: Boolean, // Always showing
-    multiline: Boolean, // Multiple lines
-    underlined: Boolean,
-    abbreviation: Boolean,
-    position: {
-      type: String,
-      default: 'is-top',
-      validator(value) {
-        return ['is-top', 'is-bottom', 'is-left', 'is-right'].indexOf(value) > -1
-      },
-    },
-    size: {
-      type: String,
-      default: 'is-medium',
-      validator(value) {
-        return ['is-small', 'is-medium', 'is-large'].indexOf(value) > -1
-      },
-    },
-  },
-  mounted() {
-    console.log(this.label)
-  },
-  data() {
-    return {
-      labelText: this.label || null,
-      isActive: this.active || true,
-      isSticky: this.sticky || false,
-      isMultiline: this.multiline || false,
-      isUnderlined: this.underlined || false,
-      isAbbreviation: this.abbreviation || false,
-      hasPosition: this.position || 'is-top',
-      hasSize: this.size || 'is-medium',
-    }
-  },
-  computed: {
-    dynamicStyles() {
-      return {
-        '--vue-custom-tooltip-color':
-          this.$vueCustomTooltip && this.$vueCustomTooltip.hasOwnProperty('color')
-            ? this.$vueCustomTooltip.color
-            : null,
-        '--vue-custom-tooltip-background':
-          this.$vueCustomTooltip && this.$vueCustomTooltip.hasOwnProperty('background')
-            ? this.$vueCustomTooltip.background
-            : null,
-        '--vue-custom-tooltip-border-radius':
-          this.$vueCustomTooltip && this.$vueCustomTooltip.hasOwnProperty('borderRadius')
-            ? this.$vueCustomTooltip.borderRadius
-            : null,
-        '--vue-custom-tooltip-font-weight':
-          this.$vueCustomTooltip && this.$vueCustomTooltip.hasOwnProperty('fontWeight')
-            ? this.$vueCustomTooltip.fontWeight
-            : null,
-      }
-    },
-  },
-  watch: {
-    label: {
-      handler(value) {
-        this.labelText = value
-      },
-      immediate: true,
-    },
-    active: {
-      handler(value) {
-        this.isActive = value
-      },
-      immediate: true,
-    },
-    sticky: {
-      handler(value) {
-        this.isSticky = value
-      },
-      immediate: true,
-    },
-    multiline: {
-      handler(value) {
-        this.isMultiline = value
-      },
-      immediate: true,
-    },
-    underlined: {
-      handler(value) {
-        this.isUnderlined = value
-      },
-      immediate: true,
-    },
-    abbreviation: {
-      handler(value) {
-        this.isAbbreviation = value
-      },
-      immediate: true,
-    },
-    position: {
-      handler(value) {
-        this.hasPosition = value
-      },
-      immediate: true,
-    },
-    size: {
-      handler(value) {
-        this.hasSize = value
-      },
-      immediate: true,
-    },
-  },
-}
-</script> -->
 
+</script>
 <style lang="scss">
 $tooltip-color: v-bind(color); // color
-$tooltip-background:  v-bind(background); // background color
+$tooltip-background: v-bind(background); // background color
 $tooltip-radius: v-bind(radius); // border radius
 $tooltip-weight: v-bind(weight); // font weight
-$speed: 200ms; // animation speed
+$speed: 400ms; // animation speed
 $small: 100px; // 140
 $medium: 200px; // 250
 $large: 300px; // 480
-// $easing = ease-out
-.vue-custom-tooltip {
-	position: relative;
-	display: inline-flex; //flex;
-	text-decoration-line: none !important;
-	&.is-top {
-		&:before {
-			border-top: 8px solid $tooltip-background;
-			border-right: 5px solid transparent;
-			border-left: 5px solid transparent;
-			bottom: calc(100% + 2px);
-			position: absolute;
-			bottom: 100% !important;
-			//box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
-		}
-		&.has-multiline {
-			&.is-small {
-				&:after {
-					width: $small;
-				}
-			}
-			&.is-medium {
-				&:after {
-					width: $medium;
-					padding: 0.6rem 1.25rem 0.65rem;
-				}
-			}
-			&.is-large {
-				&:after {
-					width: $large;
-					padding: 0.6rem 1rem 0.65rem;
-				}
-			}
-		}
-	}
-	&.is-right {
-		&:before {
-			border-top: 5px solid transparent;
-			border-right: 8px solid $tooltip-background;
-			border-bottom: 5px solid transparent;
-			left: calc(100% + 2px);
-			left: 100% !important;
-		}
-		&.has-multiline {
-			&.is-small {
-				&:after {
-					width: $small;
-				}
-			}
-			&.is-medium {
-				&:after {
-					width: $medium;
-					padding: 0.6rem 1.25rem 0.65rem;
-				}
-			}
-			&.is-large {
-				&:after {
-					width: $large;
-					padding: 0.6rem 1rem 0.65rem;
-				}
-			}
-		}
-	}
-	&.is-bottom {
-		&:before {
-			border-right: 5px solid transparent;
-			border-bottom: 8px solid $tooltip-background;
-			border-left: 5px solid transparent;
-			top: calc(100% + 2px);
-	    top: 100% !important;  /* At the top of the tooltip */
-		}
-		&.has-multiline {
-			&.is-small {
-				&:after {
-					width: $small;
-				}
-			}
-			&.is-medium {
-				&:after {
-					width: $medium;
-					padding: 0.6rem 1.25rem 0.65rem;
-				}
-			}
-			&.is-large {
-				&:after {
-					width: $large;
-					padding: 0.6rem 1rem 0.65rem;
-				}
-			}
-		}
-	}
-	&.is-left {
-		&:before {
-			border-top: 5px solid transparent;
-			border-bottom: 5px solid transparent;
-			border-left: 8px solid $tooltip-background;
-			right: calc(100% + 2px);
-			right: 100% !important;
-		}
-		&.has-multiline {
-			&.is-small {
-				&:after {
-					width: $small;
-				}
-			}
-			&.is-medium {
-				&:after {
-					width: $medium;
-					padding: 0.6rem 1.25rem 0.65rem;
-				}
-			}
-			&.is-large {
-				&:after {
-					width: $large;
-					padding: 0.6rem 1rem 0.65rem;
-				}
-			}
-		}
-	}
-	&.is-underlined {
-		border-bottom: 1px dotted $tooltip-background;
-		line-height: 1.2;
-	}
-	&:before {
-		z-index: 889;
-	}
-	&:after {
-		content: attr(data-label);
-		color: $tooltip-color;
-		background: $tooltip-background;
-		width: auto;
-		max-width: 100vw;
-		padding: 0.35rem 0.75rem 0.45rem;
-		border-radius: $tooltip-radius;
-		font-size: 0.85rem !important;
-		font-weight: $tooltip-weight;
-		line-height: 1.3;
-		letter-spacing: normal !important;
-		text-transform: none;
-		box-shadow: 0px 1px 2px 1px rgba(0,1,0,0.2);
-		z-index: 888;
-		white-space: nowrap;
-	}
-	&.has-multiline {
-		&:after {
-			display: block;
-			padding: 0.5rem 0.75rem 0.65rem;
-			text-align: center;
-			line-height: 1.4;
-			white-space: pre-wrap;
-		}
-	}
-}
-.vue-custom-tooltip.is-top:before,
-.vue-custom-tooltip.is-top:after {
-	top: auto;
-	right: auto;
-	bottom: calc(100% + 5px + 2px);
-	left: 50%;
-	transform: translateX(-50%);
-}
-.vue-custom-tooltip.is-right:before,
-.vue-custom-tooltip.is-right:after {
-	top: 50%;
-	right: auto;
-	bottom: auto;
-	left: calc(100% + 5px + 2px);
-	transform: translateY(-50%);
-}
-.vue-custom-tooltip.is-bottom:before,
-.vue-custom-tooltip.is-bottom:after {
-	top: calc(100% + 5px + 2px);
-	right: auto;
-	bottom: auto;
-	left: 50%;
-	transform: translateX(-50%);
-}
-.vue-custom-tooltip.is-left:before,
-.vue-custom-tooltip.is-left:after {
-	top: 50%;
-	right: calc(100% + 5px + 2px);
-	bottom: auto;
-	left: auto;
-	transform: translateY(-50%);
-}
-.vue-custom-tooltip:before,
-.vue-custom-tooltip:after {
-	position: absolute;
-	content: '';
-	opacity: 0;
-	visibility: hidden;
-	pointer-events: none;
-	transition: opacity $speed ease-in-out, visibility $speed ease-in-out;
-	animation: wobbles 0.8s ease;
-	font-family: v-bind(font);
-}
-.vue-custom-tooltip:after {
-  content: attr(data-label);
-}
-.vue-custom-tooltip:not([data-label='']):hover:before,
-.vue-custom-tooltip:not([data-label='']):hover:after {
-	opacity: 1;
-	visibility: visible;
-}
-:disabled {
-	.vue-custom-tooltip {
-		pointer-events: none;
-	}
-}
-.vue-custom-tooltip:not([data-label='']).is-sticky:before,
-.vue-custom-tooltip:not([data-label='']).is-sticky:after {
-	opacity: 1;
-	visibility: visible;
+$height: v-bind(height);
+$width: v-bind(width);
+
+.tooltip-container {
+  position: relative;
+  //display: flex;
+  &.is-underlined {
+    border-bottom: 1px dotted $tooltip-background;
+    line-height: 1.2;
+  }
 }
 
-.wobble-enter-active {
-  animation: wobbles 0.8s ease;
+.tooltip-container:hover .cadooz-tooltip {
+  opacity: 1;
+  visibility: visible;
+  //width: $large;
 }
-.wobble-leave-active {
-  transition: all 1s ease;
-  /* animation: wobbles 1s linear; */
+.not-active {
+  display: none;
 }
-@keyframes wobbles {
-  0% {
-    transform: translateY(-20px);
-    opacity: 0;
+.cadooz-tooltip {
+  position: absolute;
+  color: $tooltip-color;
+  text-align: center;
+  padding: 1em 0.5em;
+  visibility: hidden;
+  pointer-events: none;
+  opacity: 0;
+  //width: 0;
+  transition: opacity $speed ease-in-out, visibility $speed ease-in-out, width $speed ease-in-out;
+  z-index: 100;
+  background: $tooltip-background;
+  border-radius: $tooltip-radius;
+  font-size: 0.85rem !important;
+  font-weight: $tooltip-weight;
+  font-family: v-bind(font);
+
+  //width: auto;
+  max-width: 100vw;
+  //white-space: nowrap;
+  line-height: 1.3;
+  letter-spacing: normal !important;
+  text-transform: none;
+  /* box-shadow: rgba(0, 0, 0, 0.3) 0 2px 10px; */
+
+
+  &.has-shadow {
+    box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.75);
+    filter: drop-shadow(rgba(0, 0, 0, 0.3) 0 2px 7px);
   }
-  50% {
-    transform: translateY(0px);
-    opacity: 0.3;
+
+  &.is-small {
+    width: $small;
+    //white-space: break-spaces;
   }
-  60% {
-    transform: translateX(8px);
-    opacity: 0.3;
+  &.is-medium {
+    width: $medium;
   }
-  70% {
-    transform: translateX(-8px);
-    opacity: 0.7;
+  &.is-large {
+    width: $large;
   }
-  80% {
-    transform: translateX(4px);
-    opacity: 0.7;
+  &.is-top,
+  &.is-bottom {
+    transform: translate(-50%);
+    left: 50%;
   }
-  90% {
-    transform: translateX(-4px);
-    opacity: 1;
+  &.is-top {
+    bottom: calc(100% + $height - 5px);
   }
-  100% {
-    transform: translateX(0px);
-    opacity: 1;
+  &.is-bottom {
+    top: calc(100% + $height - 5px);
+  }
+  &.is-left,
+  &.is-right {
+    top: -50%;
+  }
+  &.is-left {
+    right: calc(100% + 10px);
+  }
+  &.is-right {
+    left: calc(100% + $width);
+  }
+}
+
+.cadooz-tooltip__arrow {
+  position: absolute;
+  /* box-shadow: 12px 0 15px -4px rgba(31, 73, 125, 0.8), -12px 0 8px -4px rgba(31, 73, 125, 0.8); */
+  rect {
+    fill: $tooltip-background;
+  }
+  &.has-shadow {
+    box-shadow: 2px 2px rgba(0, 0, 0, 0.2);
+  }
+  &.is-right,
+  &.is-left {
+    top: calc(50% - 5px);
+  }
+  &.is-right {
+    left: calc(0% - 5px);
+    transform: rotate(135deg);
+  }
+  &.is-left {
+    left: calc(100% - 5px);
+    transform: rotate(-45deg);
+  }
+  &.is-bottom,
+  &.is-top {
+    left: calc(50% - 5px);
+  }
+  &.is-bottom {
+    top: calc(0% - 5px);
+    transform: rotate(-135deg);
+  }
+  &.is-top {
+    top: calc(100% - 5px);
+    transform: rotate(45deg);
   }
 }
 </style>
