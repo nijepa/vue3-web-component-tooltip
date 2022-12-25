4b8470a3ac3c40fd59422acf6be3db2a96174d75
Nikola Pavicevic <nijepa@gmail.com>
2022-12-25 01:52:31 +0100

    fixed position approach

--- components/Custom-Tooltip.ce.vue
+++ components/Custom-Tooltip.ce.vue
@@ -1,26 +1,18 @@
 <template>
-  <component
-    class="cadooz-tooltip__container"
-    :is="isAbbreviation ? 'abbr' : 'span'"
-    :class="{ 'is-underlined': isUnderlined || isAbbreviation }"
-    :style="{ cursor: isAbbreviation ? 'help' : 'pointer' }"
-  >
-    <span
+  <transition name="wobble" appear mode="in-out">
+    <div
+      ref="tooltip"
+      v-if="isActive"
       :class="[
         hasPosition,
         hasSize,
         hasShadow,
         {
           'cadooz-tooltip': isActive && labelText,
-          'is-sticky': isSticky,
-          'has-multiline': isMultiline,
-          'is-underlined': isUnderlined || isAbbreviation,
         },
       ]"
     >
-      <span class="text" :class="{ 'not-active': !isActive || !labelText }">{{
-        labelText
-      }}</span>
+      <span class="text">{{ labelText }}</span>
       <svg
         width="10"
         height="10"
@@ -33,33 +25,20 @@
       >
         <rect width="10" height="10" />
       </svg>
-    </span>
-    <slot />
-  </component>
+    </div>
+  </transition>
 </template>
 <script setup>
-import { ref, computed, watch, onMounted, useAttrs } from "vue";
-
+import { ref, computed, onMounted, nextTick, useAttrs } from "vue";
 // setting props
 const props = defineProps({
   active: {
     type: String,
-    default: "true",
+    default: "false",
   },
   label: {
     type: String,
-  },
-  sticky: {
-    type: Boolean,
-  },
-  multiline: {
-    type: Boolean,
-  },
-  underlined: {
-    type: Boolean,
-  },
-  abbreviation: {
-    type: Boolean,
+    default: "tooltip",
   },
   position: {
     type: String,
@@ -77,11 +56,11 @@ const props = defineProps({
   },
   background: {
     type: String,
-    default: "#fff",
+    default: "#000",
   },
   color: {
     type: String,
-    default: "#000",
+    default: "#fff",
   },
   font: {
     type: String,
@@ -91,7 +70,7 @@ const props = defineProps({
     type: String,
     default: "1em",
   },
-  weight: {
+  fontweight: {
     type: Number,
     default: 400,
   },
@@ -105,77 +84,117 @@ const props = defineProps({
   },
 });
 
-const attrs = useAttrs();
-const elem = ref(document.querySelector("#" + attrs.id));
-
 const labelText = ref(props.label || null);
 const isActive = ref(props.active === "true");
-const isSticky = ref(props.sticky || false);
-const isMultiline = ref(props.multiline || false);
-const isUnderlined = ref(props.underlined || false);
-const isAbbreviation = ref(props.abbreviation || false);
 const hasPosition = ref(props.position || "is-top");
 const hasSize = ref(props.size || "is-medium");
 const hasBackground = ref(props.background);
 const hasColor = ref(props.color);
 const hasFont = ref(props.font);
 const hasFontSize = ref(props.fontsize);
+const hasFontWeight = ref(props.fontweight);
 const hasRadius = ref(props.radius);
+const top = ref(0);
+const left = ref(0);
+const clientWidth = ref(0);
+const clientHeight = ref(0);
+const tooltip = ref(null);
 
 const hasShadow = computed(() =>
   props.shadow === "false" ? "" : "has-shadow"
 );
+const getText = (e) => {
+  labelText.value = e.getAttribute("data-label") || props.label;
+}
 
-const hasHeight = ref(null);
-const hasWidth = ref(null);
+const getOffset = (el) => {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY,
+    right: rect.right + window.scrollX,
+    bottom: rect.bottom + window.scrollY,
+  };
+};
+const getWidth = () => {
+  return hasSize.value === "is-medium"
+    ? 200
+    : hasSize.value === "is-small"
+    ? 100
+    : 300;
+};
+const getHeight = () => {
+  clientHeight.value = tooltip.value?.offsetHeight + 10;
+};
+const getWeight = (e) => {
+  clientWidth.value = getOffset(e).right - getOffset(e).left;
+}
+
+const getPosition = {
+  "is-top": (e) => {
+    top.value = `${getOffset(e).top - clientHeight.value}px`;
+    left.value = `${
+      getOffset(e).left + clientWidth.value / 2 - getWidth() / 2 - 10
+    }px`;
+  },
+  "is-bottom": (e) => {
+    top.value = `${getOffset(e).bottom + 10}px`;
+    left.value = `${
+      getOffset(e).left + clientWidth.value / 2 - getWidth() / 2 - 10
+    }px`;
+  },
+  "is-left": (e) => {
+    top.value = `${
+      getOffset(e).top + (getOffset(e).bottom - getOffset(e).top) / 2 - 20
+    }px`;
+    left.value = `${getOffset(e).left - getWidth() - 25}px`;
+  },
+  "is-right": (e) => {
+    top.value = `${
+      getOffset(e).top + (getOffset(e).bottom - getOffset(e).top) / 2 - 20
+    }px`;
+    left.value = `${
+      getOffset(e).left + (getOffset(e).right - getOffset(e).left) + 10
+    }px`;
+  },
+};
+
+const show = (e) => {
+  isActive.value = true;
+  nextTick(() => {
+    getText(e)
+    getHeight();
+    getWeight(e);
+    getPosition[hasPosition.value](e);
+  });
+};
+const hide = () => {
+  isActive.value = false;
+};
+
 onMounted(() => {
-  hasHeight.value = elem.value?.children[0]?.scrollHeight + "px";
-  hasWidth.value = elem.value?.children[0]?.scrollWidth + "px";
+  let tooltips = document.querySelectorAll("[cadooz-tooltip]");
+  tooltips.forEach((c) => {
+    c.addEventListener("mouseover", function () {
+      show(c);
+    });
+    c.addEventListener("mouseleave", function () {
+      hide(c);
+    });
+  });
 });
-
-watch(
-  () => props.active,
-  (newValue, oldValue) => {
-    console.info(
-      "Watch props.active function called with args:",
-      newValue,
-      oldValue
-    );
-    isActive.value = newValue === "true";
-  }
-);
 </script>
 <style lang="scss">
-$tooltip-color: v-bind(hasColor);           // color
+$tooltip-color: v-bind(hasColor); // color
 $tooltip-background: v-bind(hasBackground); // background color
-$tooltip-radius: v-bind(hasRadius);         // border radius
-$tooltip-weight: v-bind(weight);            // font weight
-$speed: 400ms;                              // animation speed
-$small: 100px;                              // 140
-$medium: 200px;                             // 250
-$large: 300px;                              // 480
-$height: v-bind(hasHeight);
-$width: v-bind(hasWidth);
-
-.cadooz-tooltip__container {
-  position: relative;
-  &.is-underlined {
-    border-bottom: 1px dotted $tooltip-background;
-    line-height: 1.2;
-  }
-  &:hover .cadooz-tooltip {
-    opacity: 1;
-    visibility: visible;
-  }
-}
-
-.not-active {
-  display: none;
-}
+$tooltip-radius: v-bind(hasRadius); // border radius
+$tooltip-weight: v-bind(hasFontWeight); // font weight
+$speed: 400ms; // animation speed
+$small: 100px; // 140
+$medium: 200px; // 250
+$large: 300px; // 480
 .cadooz-tooltip {
-  position: absolute;
-  visibility: hidden;
-  opacity: 0;
+  position: fixed;
   z-index: 100;
   padding: 0.75em 0.5em;
   pointer-events: none;
@@ -189,46 +208,21 @@ $width: v-bind(hasWidth);
   font-family: v-bind(hasFont);
   font-size: v-bind(hasFontSize);
   font-weight: $tooltip-weight;
-  transition: opacity $speed ease-in-out, visibility $speed ease-in-out;
-  //white-space: nowrap;
-  /* box-shadow: rgba(0, 0, 0, 0.3) 0 2px 10px; */
-
+  //transition: opacity $speed ease-in-out, visibility $speed ease-in-out;
+  top: v-bind(top);
+  left: v-bind(left);
   &.has-shadow {
     box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.75);
     filter: drop-shadow(rgba(0, 0, 0, 0.3) 0 2px 7px);
   }
-
   &.is-small {
     width: $small;
-    //white-space: break-spaces;
   }
   &.is-medium {
     width: $medium;
   }
   &.is-large {
     width: $large;
-  }
-  &.is-top,
-  &.is-bottom {
-    transform: translate(-50%);
-    left: 50%;
-  }
-  &.is-top {
-    bottom: calc(100% + $height - 5px);
-  }
-  &.is-bottom {
-    top: calc(100% + $height - 5px);
-  }
-  &.is-left,
-  &.is-right {
-    //transform: translateY(50%);
-    top: calc(-50% + 5px);
-  }
-  &.is-left {
-    right: calc(100% + 10px);
-  }
-  &.is-right {
-    left: calc(100% + $width + 10px);
   }
 }
 .cadooz-tooltip__arrow {
@@ -262,6 +256,22 @@ $width: v-bind(hasWidth);
   &.is-top {
     top: calc(100% - 5px);
     transform: rotate(45deg);
+  }
+}
+.wobble-enter-active {
+  animation: wobbles $speed ease;
+}
+.wobble-leave-active {
+  animation: wobbles $speed reverse;
+}
+@keyframes wobbles {
+  0% {
+    transform: translateY(75px) scale(0.6, 0.2) skew(30deg, 20deg);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0px) scale(1, 1) skew(0deg, 0deg);
+    opacity: 1;
   }
 }
 </style>
